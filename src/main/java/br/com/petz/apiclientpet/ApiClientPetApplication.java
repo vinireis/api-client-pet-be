1d0c2ca8fc5a2d2c908a866f37e7fd0d1d4cedb5
Vinicius Reis <vinicius.reis@hdi.com.br>
2020-08-23 10:51:18 -0300

    adiciona swagger spring fox to api

--- src/main/java/br/com/petz/apiclientpet/ApiClientPetApplication.java
+++ src/main/java/br/com/petz/apiclientpet/ApiClientPetApplication.java
@@ -3,7 +3,10 @@ package br.com.petz.apiclientpet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class ApiClientPetApplication {
 
 	public static void main(String[] args) {
