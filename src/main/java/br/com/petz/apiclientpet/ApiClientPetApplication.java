5496ee9a24182f38e701baaf5fbbd28db9123310
Vinicius Reis <vinicius.reis@hdi.com.br>
2020-08-23 10:37:09 -0300

    initial commit - spring initializr

---
+++ src/main/java/br/com/petz/apiclientpet/ApiClientPetApplication.java
@@ -0,0 +1,13 @@
+package br.com.petz.apiclientpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiClientPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiClientPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/petz/apiclientpet/ApiClientPetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.petz.apiclientpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClientPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
